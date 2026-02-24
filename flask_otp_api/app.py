26de83f7b1c90b0a0a5c5313eaa812864afd6a39
Kennyk1 <kennyfavour11@gmail.com>
2026-02-24 07:32:20 +0100

    Update app.py

--- flask_otp_api/app.py
+++ flask_otp_api/app.py
@@ -747,5 +747,5 @@ def get_announcement():
 
 if __name__ == "__main__":
     print("✅ APP RUNNING ON PORT", os.environ.get("PORT", 5000))
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
